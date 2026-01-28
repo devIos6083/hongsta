543d3853de796292e54bdb4025d7185b7a8d61fc
Corsair <khkyu9799@ajou.ac.kr>
2026-01-28 21:05:13 +0900

    update 데이터정규화,낙관화

--- pages/todo-list-page.tsx
+++ pages/todo-list-page.tsx
@@ -1,9 +1,9 @@
 import TodoEditor from "@/components/todo-list/todo-editor";
 import TodoItem from "@/components/todo-list/todo-item";
-import { useTodosData } from "@/hooks/quries/use-todos-data";
+import { useTodosData } from "@/hooks/queries/use-todos-data";
 
 export default function TodoListPage() {
-  const { data: todos, isLoading, error } = useTodosData();
+  const { data: todoIds, isLoading, error } = useTodosData();
 
   if (error) return <div>오류가 발생했습니다.</div>;
   if (isLoading) return <div>로딩 중 입니다 ...</div>;
@@ -11,9 +11,9 @@ export default function TodoListPage() {
   return (
     <div className="flex flex-col gap-5 p-5">
       <h1 className="text-2xl font-bold">TodoList</h1>
-      <TodoEditor id={0} content={""} />
-      {todos?.map((todo: { id: any }) => (
-        <TodoItem content={""} key={todo.id} {...todo} />
+      <TodoEditor />
+      {todoIds?.map((id) => (
+        <TodoItem key={id} id={id} />
       ))}
     </div>
   );
