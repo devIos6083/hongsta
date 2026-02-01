1d8607a895b7c230209bde89e00e49ababc154bb
Corsair <khkyu9799@ajou.ac.kr>
2026-02-01 22:46:55 +0900

    update 회원가입

--- components/counter/viewer.tsx
+++
@@ -1,7 +0,0 @@
-import { useCount } from "@/store/count";
-
-export default function Viewer() {
-  const count = useCount();
-
-  return <div>{count}</div>;
-}
---
+++ pages/reset-password-page.tsx
@@ -0,0 +1,4 @@
+export default function ResetPasswordPage() {
+  return <div>ResetPasswordPage</div>;
+}
+ 
